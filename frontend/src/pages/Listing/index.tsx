7548f46028c10ecf9194d0dad60210bad2c4d130
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-05-09 15:17:08 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,6 +5,7 @@ function Listing() {
   return (
     <>
       <Pagination />
+
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
