a977db474cc94bf0da72a4a40c1ea207523a5a6d
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-05-09 14:30:08 -0300

    Routes configured

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,5 @@
+function Form() {
+  return <h1>Página de Formulário</h1>;
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,5 @@
+function Listing() {
+  return <h1>Página de Listagem</h1>;
+}
+
+export default Listing;
